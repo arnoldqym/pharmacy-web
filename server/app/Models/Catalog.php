d3358f050c4fceae5de802fa10a0d0f34ad1b519
Arnold Kimani <deathqqq01@gmail.com>
2025-12-22 16:09:25 +0300

    created fillable and routing for catalog model

--- server/app/Models/Catalog.php
+++ server/app/Models/Catalog.php
@@ -6,5 +6,14 @@ use Illuminate\Database\Eloquent\Model;
 
 class Catalog extends Model
 {
-    //
+    protected $table = 'categories';
+    protected $fillable = [
+        'name',
+        'active',
+        'meta_title',
+        'meta_description',
+        'meta_keywords'
+
+    ];
+
 }
