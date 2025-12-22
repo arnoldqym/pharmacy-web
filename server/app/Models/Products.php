b9da1895392ef2c994943158fb43dca64f083a03
Arnold Kimani <deathqqq01@gmail.com>
2025-12-22 16:07:15 +0300

    created fillable and routing for product model

--- server/app/Models/Products.php
+++ server/app/Models/Products.php
@@ -2,9 +2,36 @@
 
 namespace App\Models;
 
+use App\Models\Catalog;
 use Illuminate\Database\Eloquent\Model;
 
 class Products extends Model
 {
-    //
+
+protected $table = 'products';
+protected $fillable = [
+        'title',
+        'description',
+        'selling_price',
+        'wholesale_price',
+        'image_url',
+        'stock_quantity',
+        'category_id',
+        'active',
+        'meta_title',
+        'meta_description',
+        'meta_keywords',
+        'created_at',
+        'updated_at'
+    ];
+
+        public function Product()
+    {
+        return $this->belongsTo('App\Models\Product');
+    }
+
+    public function category()
+    {
+        return $this->belongsTo(Catalog::class, 'category_id');
+    }
 }
