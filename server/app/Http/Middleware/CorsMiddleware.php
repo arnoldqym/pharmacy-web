3570927258606488401e2b0efc2c02f82c19df75
Nick <nicksonogm@gmail.com>
2025-12-22 11:37:13 +0300

    added a test route and added more allowed origins accepted by the server

--- server/app/Http/Middleware/CorsMiddleware.php
+++ server/app/Http/Middleware/CorsMiddleware.php
@@ -21,7 +21,7 @@ class CorsMiddleware
         $origin = $request->headers->get('Origin');
 
         // Allow only your frontend origin during dev
-        if ($origin === 'http://localhost:5173') {
+        if ($origin === 'http://localhost:5173' || $origin ==='http://localhost:5174' ||  $origin === 'http://productionurl.com') {
             $response->headers->set('Access-Control-Allow-Origin', $origin);
             $response->headers->set('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
             $response->headers->set('Access-Control-Allow-Headers', 'Content-Type, Authorization');
