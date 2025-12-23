dbb550c685aa21b4439df6b625b7f2aea6602541
Nick <nicksonogm@gmail.com>
2025-12-23 09:44:43 +0300

    routes now working

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom/client'
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.jsx'
+import App from './App.tsx'
 
 //redux imports
 import { Provider } from 'react-redux'
