3707f64b833fef1931e2d390a8a3d0f471a8506a
NixonOjiem <nicksonogm@gmail.com>
2026-02-14 11:28:08 +0300

    removed bloadtware from Daashboard page
    Co-authored-by: Arnold Kimani <arnoldqym@gmail.com>

--- client/src/pages/DashboardPage.tsx
+++ client/src/pages/DashboardPage.tsx
@@ -5,8 +5,6 @@ import { AppDispatch } from "../redux/store";
 //components
 import DashboardLayout from "../layouts/DashboardLayout";
 import { Outlet } from "react-router-dom";
-// import StatsComponent from "../components/dashboard/StatsComponent";
-// import InventoryComponent from "../components/dashboard/InventoryComponent";
 
 function DashboardPage() {
   const dispatch = useDispatch<AppDispatch>();
@@ -18,15 +16,7 @@ function DashboardPage() {
 
   return (
     <DashboardLayout onLogout={handleLogout} title="Dashboard">
-      {/* Everything placed inside DashboardLayout here becomes the {children} 
-        prop and is rendered in the main gray area.
-      */}
-
-      {/* <StatsComponent />
-      <InventoryComponent /> */}
       <Outlet />
-
-      {/* Placeholder for future components */}
     </DashboardLayout>
   );
 }
