c882234200e676ddebcabfaa1843f04d8cbfedf0
NixonOjiem <nicksonogm@gmail.com>
2026-02-18 13:26:22 +0300

    created a controller for orders and api endpoint in the server
    Co-authored-by: Arnold Kimani <arnoldqym@gmail.com>

--- server/routes/api.php
+++ server/routes/api.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\Auth\AuthController;
 use App\Http\Controllers\Inventory\InventoryController;
+use App\Http\Controllers\Orders\OrderController;
 use App\Http\Controllers\Overview\OverviewController;
 use App\Http\Controllers\Upload\UploadController;
 use Illuminate\Support\Facades\Route;
@@ -39,4 +40,7 @@ Route::middleware('auth:api')->group(function () {
     //fetch inventory
     Route::get('/inventory', [InventoryController::class, 'index'])->name('inventory.index');
     Route::get('/overview', [OverviewController::class, 'index'])->name('overview.index');
+
+    //process orders
+    Route::post('/orders', [OrderController::class, 'index'])->name('orders.index');
 });
