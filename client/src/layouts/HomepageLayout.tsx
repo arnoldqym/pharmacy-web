a3001d27a13a2311361080cd779a28f94380740b
Arnold Kimani <arnoldqym@gmail.com>
2026-02-18 13:50:40 +0300

    footer designed and fully responsive
    Co-authored-by: Nixon Ojiem <nicksonogm@gmail.com>

--- client/src/layouts/HomepageLayout.tsx
+++ client/src/layouts/HomepageLayout.tsx
@@ -1,12 +1,15 @@
-import React from 'react'
-import NavbarComponent from '../components/Homepage/NavbarComponent'
-import FooterComponent from '../components/Homepage/FooterComponent'
+import React from 'react';
+import NavbarComponent from '../components/Homepage/NavbarComponent';
+import FooterComponent from '../components/Homepage/FooterComponent';
 
-export default function HomepageLayout() {
+export default function HomepageLayout({ children }) {
   return (
-    <>
-        <NavbarComponent />
-        <FooterComponent />
-    </>
-  )
+    <div className="flex flex-col min-h-screen">
+      <NavbarComponent />
+      <main className="grow">
+        {children}
+      </main>
+      <FooterComponent />
+    </div>
+  );
 }
