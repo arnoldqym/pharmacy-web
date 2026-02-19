0549d3d06a109e2fd02a762d746c601a08174451
Arnold Kimani <arnoldqym@gmail.com>
2026-02-19 16:43:37 +0300

    updated pricing Section removed white background
    Co-authored-by: Nixon Ojiem <nicksonogm@gmail.com>

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,10 +1,12 @@
 import HomepageLayout from "../layouts/HomepageLayout";
 import HeroSection from "../components/Homepage/HeroSection";
+import DemoSection from "../components/Homepage/DemoSection";
 import PricingSection from "../components/Homepage/PricingSection";
 
 function LandingPage() {
   return <HomepageLayout>
     <HeroSection />
+    <DemoSection />
     <PricingSection />
   </HomepageLayout>;
 }
