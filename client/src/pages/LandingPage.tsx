b2c728b9e6d009918d82f3f2a6abfe2fb1ff0db7
Arnold Kimani <arnoldqym@gmail.com>
2026-02-20 12:54:13 +0300

    created and designed about section
    Co-authored-by: Nixon Ojiem <nicksonogm@gmail.com>

--- client/src/pages/LandingPage.tsx
+++ client/src/pages/LandingPage.tsx
@@ -1,12 +1,14 @@
 import HomepageLayout from "../layouts/HomepageLayout";
 import HeroSection from "../components/Homepage/HeroSection";
 import DemoSection from "../components/Homepage/DemoSection";
+import AboutSection from "../components/Homepage/AboutSection";
 import PricingSection from "../components/Homepage/PricingSection";
 
 function LandingPage() {
   return <HomepageLayout>
     <HeroSection />
     <DemoSection />
+    <AboutSection />
     <PricingSection />
   </HomepageLayout>;
 }
