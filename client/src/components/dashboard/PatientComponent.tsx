0d42bffabdeab011bc7a88b4dfc0c2653d7e990f
NixonOjiem <nicksonogm@gmail.com>
2026-02-27 10:53:53 +0300

    able to fetch patients in the frontend
    Co-authored-by: Arnold Kimani <arnoldqym@gmail.com>

--- client/src/components/dashboard/PatientComponent.tsx
+++ client/src/components/dashboard/PatientComponent.tsx
@@ -1,6 +1,35 @@
-import React from "react";
+import React, { useEffect } from "react";
 
 function PatientComponent() {
+  const apiUrl = import.meta.env.VITE_BASE_API_URL;
+  const fetchAllPatientsApi = `${apiUrl}/patients`;
+  const searchPatientsApi = `${apiUrl}/patients/search`;
+  const updatePatientInformationApi = `${apiUrl}/patient-update`;
+
+  const [patients, setPatients] = React.useState([]);
+
+  const fetchAllPatients = async () => {
+    try {
+      const response = await fetch(fetchAllPatientsApi, {
+        method: "GET",
+        headers: {
+          Authorization: `Bearer ${localStorage.getItem("token")}`,
+          Accept: "application/json",
+        },
+      });
+      const data = await response.json();
+      setPatients(data);
+    } catch (err) {
+      console.error("Error fetching patients:", err);
+    }
+  };
+
+  useEffect(() => {
+    fetchAllPatients();
+  }, []);
+
+  console.log("Patients:", patients);
+
   return <div>PatientComponent</div>;
 }
 
